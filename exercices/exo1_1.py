45080df3c21df485d59d213b29f0b458708b3686
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-04 16:36:42 +0200

    maj file 1

--- exercices/exo1_1.py
+++ exercices/exo1_1.py
@@ -6,18 +6,25 @@ import time
 chiffre_secret = random.randint(1,100)
 compteur_tentative=0
 now = time.time()
+indice = 3
 
 print("Bienvenue dans chiffre secret !")
 print("essaye de trouver le nombre secret en peu de tentative")
+print("tu peux utiliser des indices en tapant i, 3 indice maximum")
 
 while True:
-    nombre = input("Entrez un chiffre:")
-    if type(nombre) != int:
-      print("Petit malin c'est un nombre qu'il faut :)")  
-      continue
+    try:
+        nombre = int(input("Entrez un chiffre 1-100 ou 0 pour indice:"))
+        if nombre == 0 and indice > 0:
+            print(f"Le chiffre se trouve entre {chiffre_secret-5} et {chiffre_secret+10}")
+            indice -=1
+            continue
+    except:
+        print("Petit malin c'est un nombre qu'il faut :)")  
+        continue
     compteur_tentative +=1
     if int(nombre) == chiffre_secret :
-        print(f'Super tu as trouvé en {compteur_tentative} tentative et {int(now % 60)} seconde')  
+        print(f'Super tu as trouvé en {compteur_tentative} tentative et {int(now % 60)} seconde avec {indice} indice')  
         break    
     if int(nombre) < chiffre_secret:
         print(f'Humm chiffre trop bas')
@@ -28,3 +35,5 @@ while True:
     if int(compteur_tentative) > 10:
         print(f'Humm tu es à la {compteur_tentative} tentative')
         continue
+    
+print("Tu es un champion :) !")
