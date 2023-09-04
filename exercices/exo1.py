0639543eedff727a800f6622cca323d81a43cb4b
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-04 14:39:28 +0200

    maj file

--- exercices/exo1.py
+++ exercices/exo1.py
@@ -12,7 +12,7 @@ for t in range(10):
     print(t)
 print("#########Fontion affiche mots ###########")
 entier=[0,1,2,3,4,5,6,7,8,9,10]
-mots=["salut","j'apprend","python","en","formation","coucou","pas","mal","merci","Docapost","A bientot :)"]
+mots=["salut","j'apprend","python","en","formation","c'est","pas","mal","merci","Docapost","A bientot :)"]
 def affichemots():
     for k in entier:
         print(f'func: {mots[k]}')
