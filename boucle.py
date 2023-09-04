478aa524a10b4460f3dfe7e00c27b3f4c8203bc4
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-04 14:16:03 +0200

    maj file

---
+++ boucle.py
@@ -0,0 +1,5 @@
+month ={1:"Janvier",2:"Février",3:"Mars",4:"Avril",5:"Mai",6:"Juin",7:"juillet",8:"Aout",9:"Septembre",10:"Octobre"}
+for m in month :
+    if m > 6:
+        break
+    print(f'le {m} est le mois {month[m]}')
