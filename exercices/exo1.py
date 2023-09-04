c34028742d989e9a4b166b1e5549df60cb0b869f
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-04 14:51:28 +0200

    maj file

--- exercices/exo1.py
+++ exercices/exo1.py
@@ -1,15 +1,25 @@
 #! /usr/bin/env Python3 
 t=0
 print("######### boucle fwhile ###########")
+
 while t <= 9:
     t+=1
     print(t)
+    
 print("#########boucle for ###########")
 
 for t in range(11):
     if t == 11: break
     print(t)
     
+print("#########Fonction affiche nombre ###########")   
+def displayint(counter):
+    t=0
+    while t <= counter:
+        t+=1
+        print(f'func: {t}')
+displayint(9)
+
 print("#########Fonction affiche mots ###########")
 entier=[0,1,2,3,4,5,6,7,8,9,10]
 mots=["salut","j'apprend","python","en","formation","c'est","pas","mal","merci","Docapost","A bientot :)"]
