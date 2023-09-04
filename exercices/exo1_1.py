f48d77fa5255dc04b74125a5d25e8e1ef151bcee
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-04 15:45:36 +0200

    maj file

---
+++ exercices/exo1_1.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import random
+import time
+
+chiffre_secret = random.randint(1,100)
+compteur_tentative=0
+now = time.time()
+
+print("Bienvenue dans chiffre secret !")
+print("essaye de trouver le nombre secret en peu de tentative")
+
+while True:
+    nombre = input("Entrez un chiffre:")
+    if type(nombre) == int:
+      print("Petit malin c'est un nombre qui faut :)")  
+    compteur_tentative +=1
+    if int(nombre) == chiffre_secret :
+        print(f'Super tu as trouvé en {compteur_tentative} tentative et {int(now % 60)} seconde')  
+        break    
+    if int(nombre) < chiffre_secret:
+        print(f'Humm chiffre trop bas')
+        continue
+    if int(nombre) > chiffre_secret:
+        print(f'Humm chiffre trop haut')
+        continue
+    if int(nombre) > 10:
+        print(f'Humm tu es à la {compteur_tentative} tentative')
+        continue
