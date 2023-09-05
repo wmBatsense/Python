f16add6bb0bff3b4f1a37e35ebd07873904ed940
mattarFormation <mohamed.attar@docaposte.fr>
2023-09-05 14:51:56 +0200

    Usage des fichiers  2_1

--- exercices/exo2_1.py
+++ exercices/exo2_1.py
@@ -13,6 +13,15 @@ ajout_commentaire("test.txt")
 
 def compte_ligne_fichier(file):
     f = open(file, "r")
-    print(len(f.readlines()))
-    
+    nb_ligne=0
+    nb_mots=0
+    nb_caractere=0
+    for x in f:
+        liste_de_mot= x.split(" ")
+        nb_ligne=nb_ligne+1
+        nb_mots = nb_mots+len(liste_de_mot)
+        nb_caractere = nb_caractere+len(x)
+    print(f'nombre de lignes : {nb_ligne}')
+    print(f'nombre de mots : {nb_mots}')
+    print(f'nombre de caractères : {nb_caractere}')
 compte_ligne_fichier("test.txt")
